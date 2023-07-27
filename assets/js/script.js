8683c5f6f7ab082cede4cc654b8aad8e24890796
ralstonb9 <ralstonb9@gmail.com>
2023-07-26 22:26:40 -0700

    updated css and added current weather display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
+var apiKey = "dee567190317fc8cac89fcacbcac5aeb";
 $(document).ready(function(){
     var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-    var apiKey = "dee567190317fc8cac89fcacbcac5aeb";
     showSearchHistory();
 
     function showSearchHistory() {
@@ -8,20 +8,19 @@ $(document).ready(function(){
         
     }
     
+    $("#search-button").on("click", function() {
+        var userInput = $("#search-input").val();
+    
+        if (userInput) {
+            searchForCity(userInput);
+        }
+    });
 });
 
-$("#search-button").on("click", function() {
-    var userInput = $("#search-input").val();
-
-    if (userInput) {
-        searchForCity(userInput);
-    }
-});
 
 function searchForCity(cityName) {
-    var apiCall = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
-    var apiKey = "dee567190317fc8cac89fcacbcac5aeb";
-
+    var apiCall = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
+    
     fetch(apiCall)
         .then(function(response) {
             if(!response.ok) {
@@ -29,5 +28,67 @@ function searchForCity(cityName) {
             }
             return response.json();
         })
-        // .then()
+        .then(function(data) {
+            var lat = data[0].lat
+            var lon = data[0].lon
+            var apiCall2 = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+            console.log(data)
+            fetch(apiCall2)
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log(data);
+                    var todaysWeather = document.getElementById('current-forecast');
+                    todaysWeather.style.height = 'auto'
+                    document.getElementById('current-forecast').innerHTML=
+                    `
+                    <ul>
+                    <h3>${cityName}(${data.list[0].dt_txt})</h3><img src=https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png />
+                    <li>Temp: ${data.list[0].main.temp} °F</li>
+                    <li>${data.list[0].wind.speed} MPH</li>
+                    <li>${data.list[0].main.humidity}%</li>
+                    </ul>
+                    `
+                
+                for (var i=2; i < data.list.length; i+=8) {
+                    console.log(data.list[i])
+                    document.getElementById('future').innerHTML=
+                    `
+                    <ul>
+                    <h5>${data.list[i].dt_txt}</h5><img src=https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png />
+                    <li>Temp: ${data.list[i].main.temp} °F</li>
+                    <li>${data.list[i].wind.speed} MPH</li>
+                    <li>${data.list[i].main.humidity}</li>
+                    </ul>
+                    `
+                }
+                
+                
+                    //     var currentForecast = document.getElementById("current-forecast");
+                //     var weatherDisplay = displayWeatherData(data.list[0], data.city.name, true);
+                //     currentForecast.style.height = "auto";
+                //     currentForecast.innerHTML = weatherDisplay;
+                //     for (var i=0; i < 5; i++)
+                //         var forecastDisplay = displayWeatherData(data.list[i * 8 +1], data.city.name, false);
+                })
+            // function displayWeatherData(data, cityName, isCurrent) {
+                // var windSpeed = data.wind.speed;
+                // var humidity = data.main.humidity;
+                // var temperature = data.main.temp;
+                // var date = data.dt;
+                // var weatherImage = `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`
+
+                // if (isCurrent) {
+                //     header = "<h3>" + cityName + " (" + date + ')<img src="' + weatherImage + '" alt="' + data.weather[0].description + '/><h3>';
+                // } else {
+                //     header = "<h5>" + cityName + '<img src="' + weatherImage + '" alt="' + data.weather[0].description + '" /><h5>';
+                // }
+
+                // var weatherDisplayText = header + "<p>Temp: " + temperature + " °F</p>" + "<p>Wind: " + windSpeed + " mph</p>" + "<p>Humidity: " + humidity + " %</p>";
+                // return weatherDisplayText;
+            // }
+        
+        })
+
 }
